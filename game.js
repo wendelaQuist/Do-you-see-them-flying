7e7e3cd34dd789089c0e2af49961f40dd393f7f7
wendelaQuist <wendela140200@gmail.com>
2022-02-28 14:41:08 +0100

    enemies disappear when distance between projectile is > 1

--- game.js
+++ game.js
@@ -77,12 +77,34 @@ const projectile2 = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'gree
     y: -1
 });
 
-const projectiles = []
+const projectiles = [];
+const enemies = [];
 
 function spawnEnemies() {
-    setInterval(() => {
-        console.log('go');
-    }, 1000)
+    setInterval(() => { 
+        const radius = Math.random() * (30 - 8) + 8;
+
+        let x;
+        let y;
+
+        if(Math.random() < 0.5) {
+        x         = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+        y         = Math.random() * canvas.height;
+        }
+        else{
+            x         = Math.random() * canvas.width;
+            y         = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+        }
+
+        const color     = 'green';
+
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x );
+        const velocity = {x: Math.cos(angle), y: Math.sin(angle)};
+
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+
+        console.log(enemies);
+    }, 1000); 
 }
 
 function animate() {
@@ -91,7 +113,19 @@ function animate() {
     player.draw();
     projectiles.forEach(projectile => {
         projectile.update()
-    });
+    })
+        enemies.forEach((enemy, index) => {
+            enemy.update();
+
+            projectiles.forEach((projectile, projectileIndex) => {
+            const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
+            
+            if(dist - enemy.radius - projectile.radius < 1) {
+                enemies.splice(index, 1);
+                projectiles.splice(projectileIndex, 1);
+            }
+        })
+    })
 }
 
 addEventListener('click', (event) => {
