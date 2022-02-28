4cada9884eb87621e667df341dcd2b9c915483a8
wendelaQuist <wendela140200@gmail.com>
2022-02-28 13:26:17 +0100

    bullets shoot in the  right angle

--- game.js
+++ game.js
@@ -41,11 +41,31 @@ class Projectile {
     }
 }
 
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x      = x
+        this.y      = y
+        this.radius = radius
+        this.color  = color
+        this.velocity = velocity
+    }
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+    }
+    update(){
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    }
+}
+
 const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 30, 'blue');
-player.draw();
 
 const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', {
     x: 1,
@@ -59,16 +79,26 @@ const projectile2 = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'gree
 
 const projectiles = []
 
+function spawnEnemies() {
+    setInterval(() => {
+        console.log('go');
+    }, 1000)
+}
+
 function animate() {
     requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw();
     projectiles.forEach(projectile => {
         projectile.update()
     });
 }
 
 addEventListener('click', (event) => {
-    const angle = Math.atan2(event.clientY)
-    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', {x: 1, y: 1}))
+    const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
+    const velocity = {x: Math.cos(angle), y: Math.sin(angle)};
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity))
 })
 
 animate();
+spawnEnemies();
