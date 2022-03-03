6abce0ddae49d27f55e1e2f75fe7a293eb59d85d
wendelaQuist <wendela140200@gmail.com>
2022-03-03 14:50:51 +0100

    explosion when you hit an enemy

--- game.js
+++ game.js
@@ -22,11 +22,11 @@ class Player {
 
 class Projectile {
     constructor(x, y, radius, color, velocity) {
-        this.x      = x
-        this.y      = y
-        this.radius = radius
-        this.color  = color
-        this.velocity = velocity
+        this.x          = x
+        this.y          = y
+        this.radius     = radius
+        this.color      = color
+        this.velocity   = velocity
     }
     draw() {
         c.beginPath();
@@ -43,11 +43,11 @@ class Projectile {
 
 class Enemy {
     constructor(x, y, radius, color, velocity) {
-        this.x      = x
-        this.y      = y
-        this.radius = radius
-        this.color  = color
-        this.velocity = velocity
+        this.x          = x
+        this.y          = y
+        this.radius     = radius
+        this.color      = color
+        this.velocity   = velocity
     }
     draw() {
         c.beginPath();
@@ -62,10 +62,36 @@ class Enemy {
     }
 }
 
+class Particle {
+    constructor(x, y, radius, color, velocity) {
+        this.x          = x
+        this.y          = y
+        this.radius     = radius
+        this.color      = color
+        this.velocity   = velocity
+        this.alpha      = 1
+    }
+    draw() {
+        c.save();
+        c.globalAlpha = this.alpha;
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+        c.restore();
+    }
+    update(){
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+        this.alpha -= 0.01;
+    }
+}
+
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const player = new Player(x, y, 30, 'blue');
+const player = new Player(x, y, 30, 'white');
 
 const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', {
     x: 1,
@@ -79,6 +105,7 @@ const projectile2 = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'gree
 
 const projectiles = [];
 const enemies = [];
+const particles = [];
 
 function spawnEnemies() {
     setInterval(() => { 
@@ -96,14 +123,12 @@ function spawnEnemies() {
             y         = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
         }
 
-        const color     = 'green';
+        const color     = `hsl(${Math.random() * 360}, 50%, 50%)`;
 
         const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x );
         const velocity = {x: Math.cos(angle), y: Math.sin(angle)};
 
         enemies.push(new Enemy(x, y, radius, color, velocity))
-
-        console.log(enemies);
     }, 1000); 
 }
 
@@ -111,17 +136,36 @@ let animationId;
 
 function animate() {
     animationID = requestAnimationFrame(animate);
-    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.fillStyle = 'rgba(0, 0, 0, 0.1)';
+    c.fillRect(0, 0, canvas.width, canvas.height);
     player.draw();
-    projectiles.forEach(projectile => {
+    particles.forEach((particle, index) => {
+        if(particle.alpha <= 0){
+            particles.splice(index, 1);
+        }
+        else{
+            particle.update();
+        }
+    })
+    projectiles.forEach((projectile, index) => {
         projectile.update()
+
+        if (projectile.x + projectile.radius < 0 || 
+            projectile.x - projectile.radius > canvas.width || 
+            projectile.y + projectile.radius < 0            || 
+            projectile.y - projectile.radius > canvas.height
+            ) {
+            setTimeout (() => {
+                projectiles.splice(index, 1);
+            }, 0)
+        }
     })
         enemies.forEach((enemy, index) => {
             enemy.update();
 
             const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
 
-            if(dist - enemy.radius - player.radius < 1){
+            if(dist - enemy.radius - player.radius < 1){ // Enemy collides with player
                 cancelAnimationFrame(animationID);
             }
 
@@ -129,19 +173,33 @@ function animate() {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
             
             if(dist - enemy.radius - projectile.radius < 1) {
-                setTimeout (() => {
-                    enemies.splice(index, 1);
-                    projectiles.splice(projectileIndex, 1);
-                }, 0)
+
+                for (let i=0; i < 8; i ++){ // create explosion
+                    particles.push(new Particle(projectile.x, projectile.y, 3, enemy.color, {x: Math.random() - 0.5, y:Math.random() - 0.5}));
+                }
+
+                if (enemy.radius - 10 > 10) {
+                    enemy.radius -= 10;
+                    setTimeout (() => {
+                        projectiles.splice(projectileIndex, 1);
+                    }, 0)
+                }
+                else{
+                    setTimeout (() => {
+                        enemies.splice(index, 1);
+                        projectiles.splice(projectileIndex, 1);
+                    }, 0)
+                }
             }
         })
     })
 }
 
 addEventListener('click', (event) => {
+    console.log(projectiles);
     const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
-    const velocity = {x: Math.cos(angle), y: Math.sin(angle)};
-    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity))
+    const velocity = {x: Math.cos(angle) * 5, y: Math.sin(angle) * 5};
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity))
 })
 
 animate();
